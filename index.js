3f7f6d9ec8747888b8f5d2638ac95abab3a590af
AkshatGupta2009 <110603763+AkshatGupta2009@users.noreply.github.com>
2023-07-31 18:40:15 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = express();
+router.use(cors());
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
